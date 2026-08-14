a2f3637dbe284ad4d7a059454eb276fa15372322
alfredfullstack2024 <alfredfullstack@gmail.com>
2026-08-14 14:16:45 -0500

    Update ReportesAutoridades.js

--- src/components/ReportesAutoridades.js
+++ src/components/ReportesAutoridades.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import React, { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
