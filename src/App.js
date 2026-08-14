10ef9bcc350eba4b49b147cb7e3c474d064b09dd
alfredfullstack2024 <alfredfullstack@gmail.com>
2026-08-14 09:56:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,39 +13,53 @@ import RegistroTienda from "./components/RegistroTienda";
 import CategoriaView from "./components/CategoriaView";
 import DetallesTienda from "./components/DetallesTienda";
 import Desaparecidos from "./components/Desaparecidos";
+import Salvos from "./components/Salvos";
 
 function App() {
   return (
     <Router>
       <div className="App">
+
         <Routes>
 
+          {/* Página principal */}
           <Route
             path="/"
             element={<MenuPrincipal />}
           />
 
+          {/* Registro de situación */}
           <Route
             path="/registro"
             element={<RegistroTienda />}
           />
 
+          {/* Personas no localizadas */}
           <Route
             path="/desaparecidos"
             element={<Desaparecidos />}
           />
 
+          {/* Personas a salvo */}
+          <Route
+            path="/salvos"
+            element={<Salvos />}
+          />
+
+          {/* Categorías */}
           <Route
             path="/categoria/:categoria"
             element={<CategoriaView />}
           />
 
+          {/* Detalle */}
           <Route
             path="/tienda/:id"
             element={<DetallesTienda />}
           />
 
         </Routes>
+
       </div>
     </Router>
   );
